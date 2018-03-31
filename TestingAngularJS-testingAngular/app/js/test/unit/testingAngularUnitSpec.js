56339a5aa64bc46085621d71465f442f9fc0ccb5
Tosin <kajopelayeola@gmail.com>
2018-03-31 16:43:21 -0400

    Update karma config for test coverage and karma debugging

--- TestingAngularJS-testingAngular/app/js/test/unit/testingAngularUnitSpec.js
+++ TestingAngularJS-testingAngular/app/js/test/unit/testingAngularUnitSpec.js
@@ -3,9 +3,12 @@ describe('Testing AngularJS Test Suite', function(){
   beforeEach(module('testingAngularApp'));
 
   describe('Testing AngularJS Controller', function () {
+    // Declare these variables at the top so all it() would  have access to them
     var scope, ctrl, httpBackend, timeout;
 
+    // Before each iteration make the following services available
     beforeEach(inject(function($controller, $rootScope, $httpBackend, $timeout) {
+      rootScope = $rootScope;
       scope = $rootScope.$new();
       ctrl = $controller('testingAngularCtrl', {$scope:scope});
       httpBackend = $httpBackend;
@@ -13,7 +16,8 @@ describe('Testing AngularJS Test Suite', function(){
     }));
 
     afterEach(function() {
-
+      httpBackend.verifyNoOutstandingExpectation();
+      httpBackend.verifyNoOutstandingRequest();
     });
 
     it('should initialize the title in the scope', function() {
@@ -75,30 +79,138 @@ describe('Testing AngularJS Test Suite', function(){
         city : 'Vancouver', country : 'Canada'
       }
 
-      // Mock up expected response using httpBackend and expected processed result
-      httpBackend.expectGET(weatherAPIurl + scope.destination.city + ',' + scope.destination.country + weatherAPIkey)
-      .respond({
-          weather: [{main:'Clear'},{description:'Clear sky'}],
-          main: {temp: 282.65}
-      });
-      
-      scope.getWeather(scope.destination); // Trigger ng-click getWeather(destination)
-      httpBackend.flush(); // Call or fire the httpBackend service..
-
-      expect(scope.destination.climate.weather).toBe('Clear');
-      expect(scope.destination.climate.temp).toBe(10);
-
     })
 
     it('It should display error message and remove it in time', function () {
 
-      scope.errorMsg = 'Invalid location';
-      scope.$apply(); // Trigger the digest cycle
+      rootScope.errorMsg = 'Invalid location';
+      rootScope.$apply(); // Trigger the digest cycle
       timeout.flush(); // Call timeout -- so it triggers its function
       
-      expect(scope.errorMsg).toBe(undefined);
+      expect(rootScope.errorMsg).toBe(undefined);
     })
     
   });
+
+  describe('Test warmestDestinations filter', (function () {
+    
+    // Note that  inject(function ($filter) should appear at the 
+    it('should return specified warm destinations', inject(function ($filter) {
+
+      // Mock up the kind of object that is passed through 
+      var destinations = [
+
+        {
+          city : "London",
+          country : "England",
+          climate: {
+            weather: "clouds",
+            temp: 300  
+          }
+        },
+
+        {
+          city : "Lagos",
+          country : "Nigeria",
+          climate: {
+            weather: "clear",
+            temp: 400  
+          }
+        },
+
+        {
+          city : "Moscow",
+          country : "Russia",
+          climate: {
+            weather: "snow",
+            temp: 3  
+          }
+        },
+
+        {
+          city : "Shangai",
+          country : "China",
+          climate: {
+            weather: "Haze",
+            temp: 7  
+          }
+        }
+
+      ];
+
+      // Pass the name of the filter
+      var warmest = $filter('warmestDestinations');
+      var warmestDestinations = [];
+      
+      expect(destinations.length).toBe(4);
+      expect(warmestDestinations.length).toBe(0);
+      
+      // Note that warmest(destinations, 10) returns a value
+      warmestDestinations = warmest(destinations, 10);
+
+      expect(warmestDestinations.length).toBe(2);
+      expect(warmestDestinations[0].city).toBe("London");
+      expect(warmestDestinations[1].city).toBe("Lagos");
+
+    }))
+
+  }))
+
+  describe('Test destinations directive', function () {  
+    var scope, httpBackend, template, isolateScope;
+
+    beforeEach(inject(function ($rootScope, $compile, $httpBackend) {
+      // Scope here is the parent controller's scope
+      rootScope = $rootScope;
+      scope = $rootScope.$new();
+      httpBackend = $httpBackend;
+
+      weatherAPIurl = "xyz";
+      weatherAPIkey = "xyz";
+
+      // Mock up the destination object
+      scope.destination = {
+        city: "Lagos",
+        country: "Nigeria"
+      }
+
+      var elem = angular.element(
+          '<destinations-directive'+
+            'destination="destination"'+
+            'weather-api-key="{{weatherApiKey}}"'+
+            'weather-api-url="{{weatherApiUrl}}"'+
+            'on-remove="removeDestination($index)">'+
+          '</destinations-directive>'
+      );
+      
+      template = $compile(elem)(scope);
+      scope.$digest(); // Manually trigger the digest loop
+
+      // Returns the isolate scope for the directive
+      // isolateScope = elem.isolateScope(); 
+
+    }))
+
+    // it('should update the weather for a specific destination', function () {
+      
+    //   // Mock up expected response using httpBackend and expected processed result
+    //   httpBackend.expectGET(weatherAPIurl + scope.destination.city + ',' + scope.destination.country + weatherAPIkey)
+    //   .respond({
+    //       weather: [{main:'Clear'},{description:'Clear sky'}],
+    //       main: {temp: 282.65}
+    //   });
+      
+    //   isolateScope.getWeather(scope.destination); // Trigger ng-click getWeather(destination)
+    //   httpBackend.flush(); // Call or fire the httpBackend service..
+
+    //   expect(scope.destination.climate.weather).toBe('Clear');
+    //   expect(scope.destination.climate.temp).toBe(10);
+    // })
+
+  })
+
+  
+
+
 
 });
